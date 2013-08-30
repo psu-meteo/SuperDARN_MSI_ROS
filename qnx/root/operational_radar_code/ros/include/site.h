b5fa3ecb7838a9d1c0a12485178d934245cd5f80
Jef Spaleta <jspaleta@fedoraproject.org>
2013-08-30 13:40:03 -0800

    mcm setup site specific defines

--- qnx/root/operational_radar_code/ros/include/site.h
+++ qnx/root/operational_radar_code/ros/include/site.h
@@ -1,15 +1,16 @@
 #ifndef _SITE_H
 #define _SITE_H
 
-#define ANTENNA_SEPARATION 15.24 // meters
+#define ANTENNA_SEPARATION 12.8016 // meters
 #define MAX_ANTENNAS 20
 #define MAX_TRANSMITTERS 16
 #define MAX_BACK_ARRAY 4
 #define BEAM_SPACING  3.24 //degrees
-#define MAX_RADARS 2 
-#define MAX_CHANNELS 4 
-#define GC214_PCI_INDEX -1 
-#define TIMING_PCI_INDEX 0 
+#define MAX_RADARS 1 
+#define MAX_CHANNELS 2 
+#define GC214_PCI_INDEX  0 
+#define TIMING_PCI_INDEX 1 
+#define STATUS_PORT 3
 #define GPS_DEFAULT_REFRESHRATE 1
 #define GPS_DEFAULT_TRIGRATE 10
 #endif
