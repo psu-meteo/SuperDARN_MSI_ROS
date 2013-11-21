a68fc8f084c175065438ed1648396dbce92b7fb2
Jef Spaleta <jspaleta@fedoraproject.org>
2013-11-20 23:43:57 -0100

    added function to shmem library to calculate the size of shared memory, useful for iq buffers

--- linux/home/radar/ros.3.6/codebase/base/src.lib/memory/shmem.1.2/include/shmem.h
+++ linux/home/radar/ros.3.6/codebase/base/src.lib/memory/shmem.1.2/include/shmem.h
@@ -33,5 +33,5 @@
 unsigned char *ShMemAlloc(char *memname,int size,int flags,int unlink,
 			  int *fdptr);
 int ShMemFree(unsigned char *p,char *memname,int size,int unlink,int fd);
-
+int ShMemSize(int fd);
 #endif
