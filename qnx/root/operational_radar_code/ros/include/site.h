a11afe17f8bcc6fe3f623c754787e357ba21b54a
Jef Spaleta <jspaleta@fedoraproject.org>
2013-12-24 12:51:35 -0900

    Moving site.h into site_data directory, and replacing it with a symlink to better seperate site configs from shared code

--- qnx/root/operational_radar_code/ros/include/site.h
+++ qnx/root/operational_radar_code/ros/include/site.h
@@ -1,17 +1,1 @@
-#ifndef _SITE_H
-#define _SITE_H
-
-#define ANTENNA_SEPARATION 15.24 // meters
-#define MAX_ANTENNAS 20
-#define MAX_TRANSMITTERS 16
-#define MAX_BACK_ARRAY 4
-#define BEAM_SPACING  3.24 //degrees
-#define MAX_RADARS 2 
-#define MAX_CHANNELS 4 
-#define GC214_PCI_INDEX -1 
-#define TIMING_PCI_INDEX 0 
-#define GPS_DEFAULT_REFRESHRATE 1
-#define GPS_DEFAULT_TRIGRATE 10
-#endif
-
-
+../../site_data/site.h
