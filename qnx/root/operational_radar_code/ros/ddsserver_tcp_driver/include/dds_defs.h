64c6edcf16854558761e9b69ab8a4a0f32183b15
Jef Spaleta <jspaleta@fedoraproject.org>
2014-02-12 11:08:12 -0900

    clean up dds_defs.h a bit and move site specific setting into site.h

--- qnx/root/operational_radar_code/ros/ddsserver_tcp_driver/include/dds_defs.h
+++ qnx/root/operational_radar_code/ros/ddsserver_tcp_driver/include/dds_defs.h
@@ -18,17 +18,6 @@
 #define DDS_MASTER_INDEX 0 
 #define DDS_TERM_INDEX 0 
 
-// Below set the maximum DDS output for each radar to ensure correct maximum driving signal into the transmitters with
-//  a single active radar channel
 
-// Initial Fort Hays no adjustment { East 2.5 Vp-p, West 2.0 Vp-p}
-//#define DDS_MAX_RADAR_OUTPUT { 32767, 32767 }
-
-// 2010-11-03 Fort Hays { East, West } adjusted  for 1.5 Vp-p at Transmitters
-//#define DDS_MAX_RADAR_OUTPUT { 19660,24575  }
-// 2010-11-05 Fort Hays { East, West 1.46 Vpp } adjusted  at Transmitters inputs
-//#define DDS_MAX_RADAR_OUTPUT { 18903,22477  }
-// 2011-03-08 Adjusted to { 1.8 Vpp East, 1.6 Vpp West } TX output 400-600 W_peak at 15 MHz --KTS
-#define DDS_MAX_RADAR_OUTPUT { 28355,30355 }
 
 #endif
