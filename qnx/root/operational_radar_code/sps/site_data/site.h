84eebbc68a4e81c6a6e461707729d80ffeed7938
Jef Spaleta <jspaleta@fedoraproject.org>
2014-04-01 14:58:46 -0800

    sync with sps

--- qnx/root/operational_radar_code/sps/site_data/site.h
+++ qnx/root/operational_radar_code/sps/site_data/site.h
@@ -1,15 +1,17 @@
 #ifndef _SITE_H
 #define _SITE_H
 
+/* All of this should be turned into runtime ini configs */
 #define ANTENNA_SEPARATION 15.24 // meters
 #define MAX_ANTENNAS 20
 #define MAX_TRANSMITTERS 16
 #define MAX_BACK_ARRAY 4
 #define BEAM_SPACING  3.24 //degrees
-#define MAX_RADARS 2 
-#define MAX_CHANNELS 4 
-#define GC214_PCI_INDEX -1 
-#define TIMING_PCI_INDEX 0 
+#define MAX_RADARS 1 
+#define MAX_CHANNELS 2 
+#define GC214_PCI_INDEX  0 
+#define TIMING_PCI_INDEX 1 
+#define STATUS_PORT 3
 #define GPS_DEFAULT_REFRESHRATE 1
 #define GPS_DEFAULT_TRIGRATE 10
 /* This defines the dds output level 
@@ -22,7 +24,7 @@
  * for tuned transmit performance
  * length should match MAX_RADARS 
 */ 
-#define DDS_MAX_RADAR_OUTPUT { 28355,30355 }
+#define DDS_MAX_RADAR_OUTPUT { 32000 }
 
 #endif
 
