26428958043dc499bfd697d38fa942114a7e9071
Jef Spaleta <jspaleta@fedoraproject.org>
2014-04-22 13:06:04 -0800

    variable name change use freq_hz for lookup comparison

--- qnx/root/operational_radar_code/ros/dio_tcp_driver/_select_beam.c
+++ qnx/root/operational_radar_code/ros/dio_tcp_driver/_select_beam.c
@@ -25,7 +25,7 @@ extern int32_t *f_bmnum[MAX_RADARS];
 extern double *f_lo[MAX_RADARS],*f_hi[MAX_RADARS],*f_c[MAX_RADARS];
 extern int32_t num_freqs[MAX_RADARS],num_beamcodes[MAX_RADARS],num_fsteps[MAX_RADARS],foffset[MAX_RADARS];
 
-int lookup_beamcode_by_freq(int r, double freq_mhz,double beamnm){
+int lookup_beamcode_by_freq(int r, double freq_hz,double beamnm){
   int beamcode=beamnm;
   int foff,bcodes;
   int b,best_b;
@@ -34,10 +34,10 @@ int lookup_beamcode_by_freq(int r, double freq_mhz,double beamnm){
   bcodes=num_beamcodes[r];
   best_b=-1;
   if (use_beam_table) {
-    if(freq_mhz>0) {
+    if(freq_hz>0) {
       for(b=foff;b<bcodes;b++) {
         if(f_bmnum[r][b]==beamnm) {
-          if (fabs(f_c[r][b]-freq_mhz) < fdiff) {
+          if (fabs(f_c[r][b]-freq_hz) < fdiff) {
             best_b=b;
           }
         }
@@ -103,7 +103,7 @@ int _select_beam(unsigned int base,struct ControlPRM *client){
         */
         
         int code, beamnm, temp, oldB, oldC,hi,lo;
-        double freq_mhz,angle;
+        double freq_hz,angle;
 
         unsigned int portA,portB,portC,cntl;        
         if (verbose > 1) { 
@@ -131,7 +131,7 @@ int _select_beam(unsigned int base,struct ControlPRM *client){
 		hi=hi >> 2;
         }
 
-        freq_mhz=client->tfreq*1E3;
+        freq_hz=client->tfreq*1E3;
         /* the beam code is 13 bits, pAD0 thru pAD12.  This code
            uses bits 0-7 of CH0, PortA, and bits 0-4 of CH0, PortB
            to output the beam code. Note: The beam code is an address
@@ -145,7 +145,7 @@ int _select_beam(unsigned int base,struct ControlPRM *client){
          * frequency optimal attenuatin values
          * in different bands.
          */ 
-        code=lookup_beamcode_by_freq(client->radar-1,freq_mhz,beamnm);
+        code=lookup_beamcode_by_freq(client->radar-1,freq_hz,beamnm);
 
         if (verbose > 1) 
           printf("  Selected Beamcode: %d\n",code);	
