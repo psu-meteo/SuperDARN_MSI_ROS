309114477c1598d23f4259eec23d9cfbf5465d19
bgklug <bgklug@alaska.edu>
2019-02-19 13:23:24 -0900

    Compilable testing framework

---
+++ linux/home/radar/ros.3.6/codebase/superdarn/src.lib/tk/tsg.1.13/testing/maketsg-test.cpp
@@ -0,0 +1,9 @@
+// maketsg-test.c
+// Author: Bryant Klug
+// Created: 2/19/2019
+
+#include "../include/maketsg.h"
+#include "catch.hpp"
+
+TEST_CASE("Test MakeTSG"){
+}
---
+++ linux/home/radar/ros.3.6/codebase/superdarn/src.lib/tk/tsg.1.13/testing/test-main.cpp
@@ -0,0 +1,6 @@
+// test-main.c
+// Author: Bryant Klug
+// Created: 2/19/2019
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
