7e1db90a7430afb00604077328983753fc20a963
Bill Bristow <wab5217@psu.edu>
2025-05-28 11:02:32 -0400

    Minor changes to uafscan to improve communication with usrp_server

--- linux/home/radar/ros.3.6/usr/codebase/superdarn/src.bin/os/cp/uafscan.0.01/uafscan.c
+++ linux/home/radar/ros.3.6/usr/codebase/superdarn/src.bin/os/cp/uafscan.0.01/uafscan.c
@@ -975,7 +975,8 @@ int main(int argc,char *argv[]) {
       }
       sprintf(logtxt,"Transmitting on: %d (Noise=%g)",tfreq,noise);
       ErrLog(errlog.sock,progname,logtxt);
-    
+
+      usleep(1000);
       nave=SiteIntegrate(lags);   
       if (nave<0) {
         sprintf(logtxt,"Integration error:%d",nave);
